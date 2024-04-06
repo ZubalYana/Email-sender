adac5ad2c249a143b2d0432469e95e8947eddfea
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-04-06 22:14:37 +0300

    read the emails file

---
+++ public/admin/admin.js
@@ -0,0 +1,12 @@
+// // Assuming this script is included in your 'admin.html' file
+// $(document).ready(() => {
+//     // Make an AJAX GET request to fetch file names
+//     $.get('/files', (data) => {
+//         const filesContainer = $('.filesContainer');
+
+//         // Iterate over the file names and append each to the container
+//         data.forEach((fileName) => {
+//             filesContainer.append(`<div class="filesContainer_file">${fileName}</div>`);
+//         });
+//     });
+// });
--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
 $('#file').change((e) => {
+    // let file = e.target.files[0]
     let fileName = e.target.files[0].name;
-
     axios.post('/send-name', { name: fileName })
 })
