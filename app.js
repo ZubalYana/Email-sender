44bd7c707ea8ce9e68951a4cdef2d976ff646442
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-04-06 14:38:36 +0300

    add admin panel

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require('express');
-
 const app = express();
-
+const fs = require('fs')
 const PORT = 3000;
+const path = require('path')
+
 app.use(express.static('public'));
 app.use(express.json())
 
 app.post('/send-name', (req, res) => {
     const { name } = req.body;
-    console.log(name);
+    fs.readFile(`./emails/${name}`, function(error, data){
+        if(error){
+            return console.log(error)
+        }
+        console.log(data.toString());
+    })
+})
+app.get('/admin', (req, res) => {
+    res.sendFile(__dirname + '/public/admin.html');
 })
 app.listen(PORT, () => {
     console.log(`Server work on PORT: ${PORT}`);
