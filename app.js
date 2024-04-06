9330722195d88e95308eeda473847ed5b61741dc
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-04-06 14:19:25 +0300

    read the file name

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const path = require('path')
 
 const app = express();
 
-const PORT = 3000; 
+const PORT = 3000;
+app.use(express.static('public'));
+app.use(express.json())
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+app.post('/send-name', (req, res) => {
+    const { name } = req.body;
+    console.log(name);
 })
-
-app.listen(PORT, ()=>{
-    console.log(`Server work on PORT: ${PORT}`)
-});
+app.listen(PORT, () => {
+    console.log(`Server work on PORT: ${PORT}`);
+})
---
+++ public/main.js
@@ -0,0 +1,5 @@
+$('#file').change((e) => {
+    let fileName = e.target.files[0].name;
+
+    axios.post('/send-name', { name: fileName })
+})
