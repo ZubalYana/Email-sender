3bd33d11dd25b3eeba646cc75336a8987b4c048c
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-04-06 13:53:16 +0300

    add basic fontend

--- app.js
+++ app.js
@@ -10,18 +10,6 @@ app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
-app.get('/contact', (req, res)=>{
-    res.send('Намер компанії: 0972058786');
-}) 
-
-app.get('/pay', (req, res)=>{
-    res.sendFile(path.join(__dirname, 'public', 'pay.html'))
-})
-app.get('/cont', (req, res)=>{
-    res.sendFile(path.join(__dirname, 'public', 'contact.html'))
-})
-
-
 app.listen(PORT, ()=>{
     console.log(`Server work on PORT: ${PORT}`)
 });
