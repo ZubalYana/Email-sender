d38ce3e513aa4aa23028f00c27dd4dc154867c47
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-04-08 19:55:38 +0300

    open and close the popup, add general poup settings

--- public/admin/admin.js
+++ public/admin/admin.js
@@ -3,7 +3,14 @@ axios.get('/files')
     const filesContainer = $('.filesContainer');
     response.data.forEach(fileName => {
       filesContainer.append(`<div class="filesContainer_file">${fileName}</div>`);
+      // $('.filesPopup_fileName').html(`${this.fileName}`)
     });
+    $('.filesContainer_file').click(()=>{
+      $('.filesPopup_container').css('display', 'flex')
+    })
+    $('.xmark').click(()=>{
+      $('.filesPopup_container').css('display', 'none')
+    })
   })
   .catch(error => {
     console.error('Error fetching files:', error);
